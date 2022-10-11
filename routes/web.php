437093a87ae01a503e6230da6a540baee246308d
GuilhermeMDev <guimagioni@gmail.com>
2022-10-10 22:45:11 -0300

    create db & first route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user/{user}', function (\App\Models\User $user) {
+    dd($user);
+    return $user;
+});
