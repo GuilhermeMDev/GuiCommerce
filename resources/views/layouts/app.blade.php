2eb7b7b4bd18963bbfea019aa5e8a3a6ae89cd48
GuilhermeMDev <guimagioni@gmail.com>
2022-12-05 17:26:22 -0300

    WIC: Making a litle changes and habiliting link to user edit your profile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,9 +19,9 @@
 <header class="text-gray-600 body-font">
     <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <nav class="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
-            <a href="{{ url('/') }}" class="mr-5 hover:text-gray-900">Página Principal</a>
+            <a href="{{ url('/') }}" class="mr-5 hover:text-gray-900">Início</a>
             <a class="mr-5 hover:text-gray-900">Third Link</a>
-            <a class="mr-5 hover:text-gray-900">Fourth Link</a>
+            <a class="mr-5 hover:text-gray-900">Sobre</a>
         </nav>
         <a href="{{ url('/') }}" class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
             <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
@@ -49,7 +49,8 @@
                 @endif
             @else
                 <nav class="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
-                    <a href="{{ url('/home') }}" class="mr-5 hover:text-gray-900">Admin</a>
+                    @can('edit-products') <a href="{{ url('/home') }}" class="mr-5 hover:text-gray-900">Admin</a>
+                    @else <a href="{{ url('/profile') }}" class="mr-5 hover:text-gray-900">Profile</a>@endif
                 </nav>
                 <div class="relative">
                     <span class="mr-3"> Olá {{ Auth::user()->name }},</span>
@@ -68,6 +69,7 @@
 </header>
 
 <body>
+
 <div id="app">
     @yield('content')
 </div>
