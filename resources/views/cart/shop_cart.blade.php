e0d3ffe9f31bcd7bdd9cf2a99eea978a2ca3d64b
GuilhermeMDev <guimagioni@gmail.com>
2022-12-21 18:58:10 -0300

    any changes about shop cart

--- resources/views/cart/shop_cart.blade.php
+++ resources/views/cart/shop_cart.blade.php
@@ -2,29 +2,21 @@
 
 @section('content')
     <section class="text-gray-600 body-font">
-        @if(Session::get('success'))
-            <div
-                class="bg-green-100 border border-green-400 text-green-500 px-4 py-3 rounded relative shadow-md lg:w-1/3 w-full mx-auto overflow-auto text-center"
-                role="alert">
-                <strong class="font-bold ">Produto adicionado com sucesso!</strong>
-                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                            <svg class="fill-current h-6 w-6 text-green-500" role="button"
-                                 xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path
-                                    d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+            @if(Session::get('success'))
+                <div class="bg-green-100 border border-green-400 text-green-500 px-4 py-3 rounded relative shadow-md lg:w-1/3 w-full mx-auto overflow-auto text-center" role="alert">
+                    <strong class="font-bold ">Produto adicionado com sucesso!</strong>
+                    <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                            <svg class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
                         </span>
-            </div>
-        @elseif(Session::get('delSuccess'))
-            <div
-                class="bg-red-100 border border-red-400 text-red-500 px-4 py-3 rounded relative shadow-md lg:w-1/3 w-full mx-auto overflow-auto text-center pt-6"
-                role="alert">
-                <strong class="font-bold ">Produto deletado com sucesso!</strong>
-                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                        <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg"
-                             viewBox="0 0 20 20"><title>Close</title><path
-                                d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+                </div>
+            @elseif(Session::get('delSuccess'))
+                <div class="bg-red-100 border border-red-400 text-red-500 px-4 py-3 rounded relative shadow-md lg:w-1/3 w-full mx-auto overflow-auto text-center pt-6" role="alert">
+                    <strong class="font-bold ">Produto deletado com sucesso!</strong>
+                    <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                        <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
                     </span>
-            </div>
-        @endif
+                </div>
+            @endif
         <div class="container px-50 py-24 mx-auto">
             <div class="lg:w-2/3 w-full mx-auto overflow-auto">
                 <div class="flex items-center justify-between mb-2">
@@ -73,26 +65,17 @@
                             </td>
                             <td class="px-4 py-3">{{ $item->name }}</td>
                             <td class="px-4 py-3">R$ {{ $item->price }}</td>
+                            <td class="px-4 py-3">{{ $item->quantity }}</td>
+                            <td class="px-4 py-3 text-sm text-right space-x-3 text-gray-900">
+                                <button href="{{ route('user.editShopCart') }}"
+                                   class="mt-3 text-blue-500 inline-flex items-center"><b>Alterar</b></button>
 
-                            <form action="{{ route('user.editShopCart') }}" method="POST" enctype="multipart/form-data">
-                                <td class="px-4 py-3">{{ $item->quantity }}</td>
-                                <td class="px-4 py-3 text-sm text-right space-x-3 text-gray-900">
-                                    <button class="mt-3 text-blue-500 inline-flex items-center">
-                                        <b>Alterar</b>
-                                    </button>
-
-                                    <form action="{{ route('user.delShopCart') }}" method="POST"
-                                          enctype="multipart/form-data">
-                                        <!--Estou fazendo dessa forma, pois o Carrinho nao tem um db, decidi usar uma lib externa para somar + conhecimento, porém pretendo fazer como fiz com produtos por exemplo.-->
-                                        @csrf
-                                        <input type="hidden" name="id" value="{{ $item->id}}">
-                                        <button class="mt-3 text-red-400 inline-flex items-center"><b>Remover</b>
-                                        </button>
-                                    </form>
-                                </td>
-
-                            </form>
-
+                                <form action="{{ route('user.delShopCart') }}" method="POST" enctype="multipart/form-data"> <!--Estou fazendo dessa forma, pois o Carrinho nao tem um db, decidi usar uma lib externa para somar + conhecimento, porém pretendo fazer como fiz com produtos por exemplo.-->
+                                    @csrf
+                                    <input type="hidden" name="id" value="{{ $item->id}}">
+                                   <button class="mt-3 text-red-400 inline-flex items-center"><b>Remover</b></button>
+                                </form>
+                            </td>
                         </tr>
                     @endforeach
                     </body>
@@ -100,5 +83,5 @@
             </div>
         </div>
     </section>
-    {{--    @include('components.footer')--}}
+{{--    @include('components.footer')--}}
 @endsection
