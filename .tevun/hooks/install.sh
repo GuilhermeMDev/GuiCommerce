813ef7ae5e7f99eacc52c206eba43f032dd1b849
William Correa <wilcorrea@gmail.com>
2023-01-26 23:52:19 -0300

    chore: configure CI

--- .tevun/hooks/install.sh
+++ .tevun/hooks/install.sh
@@ -7,7 +7,7 @@ cd "${1}" || exit
 # execute php deployment
 docker exec "guicommerce-nginx" bash -c "su -c \"composer install --no-interaction --optimize-autoloader --no-progress\" application"
 # execute node deployment
-docker run --rm -it -v $(pwd):/app -w /app node:16 sh -c "su -c \"npm install && npm run build\" node"
+docker run --rm -i -v $(pwd):/app -w /app node:16 sh -c "su -c \"npm install && npm run build\" node"
 
 if [[ -f ".tevun-ready" ]]; then
   docker exec "guicommerce-nginx" bash -c "php artisan migrate --force"
